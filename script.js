10d66b8b51256a351ca56cb2478cd391c4343302
atilaerkek <atilaerk@gmail.com>
2025-02-03 10:50:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,265 @@
+const okullar = {
+    anamur: [
+        { kod: "264499", ad: "Anamur Anadolu Lisesi" },
+        { kod: "750857", ad: "Anamur Cumhuriyet Anadolu Lisesi" },
+        { kod: "758234", ad: "Şehit İbrahim Armut Fen Lisesi" },
+        { kod: "764926", ad: "Anamur Şehit Yüksel Alçın Anadolu İmam Hatip Lisesi" },
+        { kod: "156923", ad: "Anamur Halk Eğitimi Merkezi" },
+        { kod: "214052", ad: "Rüştü Kazım Yücelen Mesleki Eğitim Merkezi" },
+        { kod: "966349", ad: "Anamur Rehberlik ve Araştırma Merkezi" },
+        { kod: "766166", ad: "Anamur Bilim ve Sanat Merkezi" },
+        { kod: "954384", ad: "Anamur İsmihan Çerçi Anaokulu" },
+        { kod: "975011", ad: "Akdeniz İlkokulu" },
+        { kod: "763105", ad: "Anamur Kıbrıs İlkokulu" },
+        { kod: "762360", ad: "Anamur 15 Temmuz Milli İrade İlkokulu" },
+        { kod: "700566", ad: "Anıtlı Alanoba İlkokulu" },
+        { kod: "700572", ad: "Bozdoğan Mamure İlkokulu" },
+        { kod: "700832", ad: "Çarıklar Efeler İlkokulu" },
+        { kod: "700823", ad: "Çeltikçi İlkokulu" },
+        { kod: "720951", ad: "Demirören Alan İlkokulu" },
+        { kod: "700584", ad: "Evciler Nihat Sarı İlkokulu" },
+        { kod: "974989", ad: "Fatih İlkokulu" },
+        { kod: "975005", ad: "Gercebahşiş Yenimahalle İlkokulu" },
+        { kod: "975000", ad: "Güneybahşiş İlkokulu" },
+        { kod: "700815", ad: "Güngören İlkokulu" },
+        { kod: "700868", ad: "Kalınören İlkokulu" },
+        { kod: "700728", ad: "Karalarbahşiş İlkokulu" },
+        { kod: "700799", ad: "Köprübaşı İlkokulu" },
+        { kod: "700589", ad: "Malaklar İlkokulu" },
+        { kod: "700841", ad: "Ortaköy Hidayet Sinanoğlu İlkokulu" },
+        { kod: "700576", ad: "Ovabaşı İlkokulu" },
+        { kod: "700376", ad: "Rüştü Kazım Yücelen İlkokulu" },
+        { kod: "772751", ad: "Sugözü İlkokulu" },
+        { kod: "765625", ad: "Şehit Hasan Kuş İlkokulu" },
+        { kod: "974978", ad: "Şükrü Köymen İlkokulu" },
+        { kod: "752517", ad: "Zehra Marulyalı İlkokulu" },
+        { kod: "974980", ad: "Anamur Ortaokulu" },
+        { kod: "700564", ad: "Anıtlı Alanoba Ortaokulu" },
+        { kod: "700568", ad: "Bozdoğan Mamure Ortaokulu" },
+        { kod: "700820", ad: "Çeltikçi Ortaokulu" },
+        { kod: "700816", ad: "Demirören Alan Ortaokulu" },
+        { kod: "700578", ad: "Evciler Nihat Sarı Ortaokulu" },
+        { kod: "700590", ad: "Gercebahşiş Sadet Tuna Ortaokulu" },
+        { kod: "700801", ad: "Güngören Ortaokulu" },
+        { kod: "700864", ad: "Kalınören Ortaokulu" },
+        { kod: "700597", ad: "Karalarbahşiş Ortaokulu" },
+        { kod: "700797", ad: "Köprübaşı Ortaokulu" },
+        { kod: "700586", ad: "Malaklar Ortaokulu" },
+        { kod: "700837", ad: "Ortaköy Hidayet Sinanoğlu Ortaokulu" },
+        { kod: "700574", ad: "Ovabaşı Ortaokulu" },
+        { kod: "700396", ad: "Rüştü Kazım Yücelen Ortaokulu" },
+        { kod: "765636", ad: "Şehit Şükrü Ünlü Ortaokulu" },
+        { kod: "974988", ad: "Vakıfbank Atatürk Ortaokulu" },
+        { kod: "974986", ad: "Cengiz Topel İmam Hatip Ortaokulu" },
+        { kod: "748386", ad: "Zehra Marulyalı Yatılı Bölge Ortaokulu" },
+        { kod: "963203", ad: "Abdulkadir Perşembe Vakfı Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "156888", ad: "Anamur Ticaret Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "156864", ad: "Gazi Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "340376", ad: "Valide Sultan Kız Mesleki ve Teknik Anadolu Lisesi" }
+    ],
+    bozyazi: [
+        { kod: "323504", ad: "Bozyazı Anadolu Lisesi" },
+        { kod: "751337", ad: "Bozyazı Durmuş Tufan Anadolu İmam Hatip Lisesi" },
+        { kod: "214279", ad: "Bozyazı Halk Eğitimi Merkezi" },
+        { kod: "971705", ad: "Bozyazı İlk Adım Anaokulu" },
+        { kod: "772758", ad: "Kökobası Anaokulu" },
+        { kod: "743634", ad: "Beyreli İlkokulu" },
+        { kod: "708242", ad: "Bozyazı İlkokulu" },
+        { kod: "721261", ad: "Cumhuriyet İlkokulu" },
+        { kod: "853265", ad: "Çopurlu İlkokulu" },
+        { kod: "708538", ad: "Hüseyin Hüsnü Tekışık İlkokulu" },
+        { kod: "737923", ad: "Kaledibi Şehit Turhan Kılıç İlkokulu" },
+        { kod: "708531", ad: "Kökobası İlkokulu" },
+        { kod: "721274", ad: "Kötekler İlkokulu" },
+        { kod: "708713", ad: "Lütfiye Keskin İlkokulu" },
+        { kod: "708706", ad: "Tekeli İlkokulu" },
+        { kod: "713685", ad: "Tekmen İlkokulu" },
+        { kod: "713669", ad: "Vali Çetin Birmek İlkokulu" },
+        { kod: "724230", ad: "100. Yıl Örtülü İlkokulu" },
+        { kod: "708429", ad: "Atatürk Ortaokulu" },
+        { kod: "708437", ad: "Bozyazı Ortaokulu" },
+        { kod: "708700", ad: "Gazi Ortaokulu" },
+        { kod: "708525", ad: "Kökobası Ortaokulu" },
+        { kod: "708710", ad: "Lütfiye Keskin Ortaokulu" },
+        { kod: "708416", ad: "Şehit Murat Namdar Ortaokulu" },
+        { kod: "713661", ad: "Şehit Sedat Yabalak Ortaokulu" },
+        { kod: "708702", ad: "Tekeli Ortaokulu" },
+        { kod: "713680", ad: "Tekmen Ortaokulu" },
+        { kod: "763592", ad: "Bozyazı Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "751442", ad: "Bozyazı 15 Temmuz Millî İrade Mesleki ve Teknik Anadolu Lisesi" }
+    ]
+};
+
+function ilceSecildi() {
+    const ilce = document.getElementById('ilce').value;
+    const okullarSelect = document.getElementById('okullar');
+    okullarSelect.innerHTML = '<option value="">Okul seçiniz</option>';
+    
+    if (ilce) {
+        okullar[ilce].forEach(okul => {
+            const option = document.createElement('option');
+            option.value = okul.kod;
+            option.textContent = okul.ad;
+            okullarSelect.appendChild(option);
+        });
+    }
+}
+
+function okulSecildi() {
+    const okullarSelect = document.getElementById('okullar');
+    const secilenKod = okullarSelect.value;
+    const ilce = document.getElementById('ilce').value;
+    
+    // Tüm kopyalama butonlarını seç
+    const kopyalaButonlari = document.querySelectorAll('.kopyala-btn');
+    
+    if (secilenKod && ilce) {
+        const secilenOkul = okullar[ilce].find(okul => okul.kod === secilenKod);
+        if (secilenOkul) {
+            document.getElementById('sonuc1').innerHTML = `<span class="label">Kurum Kodu: </span><span class="value">${secilenOkul.kod}</span>`;
+            document.getElementById('sonuc2').innerHTML = `<span class="label">Kurum Adı: </span><span class="value">${secilenOkul.ad}</span>`;
+            document.getElementById('sonuc3').innerHTML = `<span class="value">${secilenOkul.kod} ${secilenOkul.ad}</span>`;
+            
+            // Kopyalama butonlarını göster
+            kopyalaButonlari.forEach(btn => btn.classList.add('visible'));
+        }
+    } else {
+        document.getElementById('sonuc1').textContent = '';
+        document.getElementById('sonuc2').textContent = '';
+        document.getElementById('sonuc3').textContent = '';
+        
+        // Kopyalama butonlarını gizle
+        kopyalaButonlari.forEach(btn => btn.classList.remove('visible'));
+    }
+}
+
+function kopyala(elementId) {
+    const element = document.getElementById(elementId);
+    const button = element.nextElementSibling; // Kopyala butonunu al
+    let textToCopy;
+
+    const valueElement = element.querySelector('.value');
+    if (valueElement) {
+        textToCopy = valueElement.textContent;
+    } else {
+        textToCopy = element.textContent;
+    }
+    
+    navigator.clipboard.writeText(textToCopy).then(() => {
+        // Görsel geri bildirim
+        element.style.backgroundColor = '#e8f5e9';
+        // Buton metnini değiştir
+        button.textContent = 'Kopyalandı';
+        button.style.backgroundColor = '#45a049';
+        
+        // 2 saniye sonra eski haline döndür
+        setTimeout(() => {
+            element.style.backgroundColor = '#f9f9f9';
+            button.textContent = 'Kopyala';
+            button.style.backgroundColor = '#4CAF50';
+        }, 2000);
+    }).catch(err => {
+        console.error('Kopyalama hatası:', err);
+        // Hata durumunda butonu güncelle
+        button.textContent = 'Hata!';
+        button.style.backgroundColor = '#f44336';
+        
+        setTimeout(() => {
+            button.textContent = 'Kopyala';
+            button.style.backgroundColor = '#4CAF50';
+        }, 2000);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    okullar.anamur = [
+        { kod: "264499", ad: "Anamur Anadolu Lisesi" },
+        { kod: "750857", ad: "Anamur Cumhuriyet Anadolu Lisesi" },
+        { kod: "758234", ad: "Şehit İbrahim Armut Fen Lisesi" },
+        { kod: "764926", ad: "Anamur Şehit Yüksel Alçın Anadolu İmam Hatip Lisesi" },
+        { kod: "156923", ad: "Anamur Halk Eğitimi Merkezi" },
+        { kod: "214052", ad: "Rüştü Kazım Yücelen Mesleki Eğitim Merkezi" },
+        { kod: "966349", ad: "Anamur Rehberlik ve Araştırma Merkezi" },
+        { kod: "766166", ad: "Anamur Bilim ve Sanat Merkezi" },
+        { kod: "954384", ad: "Anamur İsmihan Çerçi Anaokulu" },
+        { kod: "975011", ad: "Akdeniz İlkokulu" },
+        { kod: "763105", ad: "Anamur Kıbrıs İlkokulu" },
+        { kod: "762360", ad: "Anamur 15 Temmuz Milli İrade İlkokulu" },
+        { kod: "700566", ad: "Anıtlı Alanoba İlkokulu" },
+        { kod: "700572", ad: "Bozdoğan Mamure İlkokulu" },
+        { kod: "700832", ad: "Çarıklar Efeler İlkokulu" },
+        { kod: "700823", ad: "Çeltikçi İlkokulu" },
+        { kod: "720951", ad: "Demirören Alan İlkokulu" },
+        { kod: "700584", ad: "Evciler Nihat Sarı İlkokulu" },
+        { kod: "974989", ad: "Fatih İlkokulu" },
+        { kod: "975005", ad: "Gercebahşiş Yenimahalle İlkokulu" },
+        { kod: "975000", ad: "Güneybahşiş İlkokulu" },
+        { kod: "700815", ad: "Güngören İlkokulu" },
+        { kod: "700868", ad: "Kalınören İlkokulu" },
+        { kod: "700728", ad: "Karalarbahşiş İlkokulu" },
+        { kod: "700799", ad: "Köprübaşı İlkokulu" },
+        { kod: "700589", ad: "Malaklar İlkokulu" },
+        { kod: "700841", ad: "Ortaköy Hidayet Sinanoğlu İlkokulu" },
+        { kod: "700576", ad: "Ovabaşı İlkokulu" },
+        { kod: "700376", ad: "Rüştü Kazım Yücelen İlkokulu" },
+        { kod: "772751", ad: "Sugözü İlkokulu" },
+        { kod: "765625", ad: "Şehit Hasan Kuş İlkokulu" },
+        { kod: "974978", ad: "Şükrü Köymen İlkokulu" },
+        { kod: "752517", ad: "Zehra Marulyalı İlkokulu" },
+        { kod: "974980", ad: "Anamur Ortaokulu" },
+        { kod: "700564", ad: "Anıtlı Alanoba Ortaokulu" },
+        { kod: "700568", ad: "Bozdoğan Mamure Ortaokulu" },
+        { kod: "700820", ad: "Çeltikçi Ortaokulu" },
+        { kod: "700816", ad: "Demirören Alan Ortaokulu" },
+        { kod: "700578", ad: "Evciler Nihat Sarı Ortaokulu" },
+        { kod: "700590", ad: "Gercebahşiş Sadet Tuna Ortaokulu" },
+        { kod: "700801", ad: "Güngören Ortaokulu" },
+        { kod: "700864", ad: "Kalınören Ortaokulu" },
+        { kod: "700597", ad: "Karalarbahşiş Ortaokulu" },
+        { kod: "700797", ad: "Köprübaşı Ortaokulu" },
+        { kod: "700586", ad: "Malaklar Ortaokulu" },
+        { kod: "700837", ad: "Ortaköy Hidayet Sinanoğlu Ortaokulu" },
+        { kod: "700574", ad: "Ovabaşı Ortaokulu" },
+        { kod: "700396", ad: "Rüştü Kazım Yücelen Ortaokulu" },
+        { kod: "765636", ad: "Şehit Şükrü Ünlü Ortaokulu" },
+        { kod: "974988", ad: "Vakıfbank Atatürk Ortaokulu" },
+        { kod: "974986", ad: "Cengiz Topel İmam Hatip Ortaokulu" },
+        { kod: "748386", ad: "Zehra Marulyalı Yatılı Bölge Ortaokulu" },
+        { kod: "963203", ad: "Abdulkadir Perşembe Vakfı Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "156888", ad: "Anamur Ticaret Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "156864", ad: "Gazi Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "340376", ad: "Valide Sultan Kız Mesleki ve Teknik Anadolu Lisesi" }
+    ];
+
+    okullar.bozyazi = [
+        { kod: "323504", ad: "Bozyazı Anadolu Lisesi" },
+        { kod: "751337", ad: "Bozyazı Durmuş Tufan Anadolu İmam Hatip Lisesi" },
+        { kod: "214279", ad: "Bozyazı Halk Eğitimi Merkezi" },
+        { kod: "971705", ad: "Bozyazı İlk Adım Anaokulu" },
+        { kod: "772758", ad: "Kökobası Anaokulu" },
+        { kod: "743634", ad: "Beyreli İlkokulu" },
+        { kod: "708242", ad: "Bozyazı İlkokulu" },
+        { kod: "721261", ad: "Cumhuriyet İlkokulu" },
+        { kod: "853265", ad: "Çopurlu İlkokulu" },
+        { kod: "708538", ad: "Hüseyin Hüsnü Tekışık İlkokulu" },
+        { kod: "737923", ad: "Kaledibi Şehit Turhan Kılıç İlkokulu" },
+        { kod: "708531", ad: "Kökobası İlkokulu" },
+        { kod: "721274", ad: "Kötekler İlkokulu" },
+        { kod: "708713", ad: "Lütfiye Keskin İlkokulu" },
+        { kod: "708706", ad: "Tekeli İlkokulu" },
+        { kod: "713685", ad: "Tekmen İlkokulu" },
+        { kod: "713669", ad: "Vali Çetin Birmek İlkokulu" },
+        { kod: "724230", ad: "100. Yıl Örtülü İlkokulu" },
+        { kod: "708429", ad: "Atatürk Ortaokulu" },
+        { kod: "708437", ad: "Bozyazı Ortaokulu" },
+        { kod: "708700", ad: "Gazi Ortaokulu" },
+        { kod: "708525", ad: "Kökobası Ortaokulu" },
+        { kod: "708710", ad: "Lütfiye Keskin Ortaokulu" },
+        { kod: "708416", ad: "Şehit Murat Namdar Ortaokulu" },
+        { kod: "713661", ad: "Şehit Sedat Yabalak Ortaokulu" },
+        { kod: "708702", ad: "Tekeli Ortaokulu" },
+        { kod: "713680", ad: "Tekmen Ortaokulu" },
+        { kod: "763592", ad: "Bozyazı Mesleki ve Teknik Anadolu Lisesi" },
+        { kod: "751442", ad: "Bozyazı 15 Temmuz Millî İrade Mesleki ve Teknik Anadolu Lisesi" }
+    ];
+}); 
